6a3643660e52052248e7415d88689785279b9602
Stella <sarahprog667@gmail.com>
2024-10-12 16:35:43 +0800

    changed version

--- core/client.py
+++ core/client.py
@@ -20,7 +20,7 @@ from core.typings import EContext
 from utils.general import emoji_context, slash_context, LOGGER_NAME
 from utils.parsers import env
 
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 
 
 class StellaEmojiBot(commands.Bot):
